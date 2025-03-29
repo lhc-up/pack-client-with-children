acd418a736d6e7cd65bb1b064b988b83d450ab4e
lhc-up <18513102276@163.com>
2025-03-29 09:54:58 +0800

    config

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const config = {
     env: 'test',
     children: [
         "questionnaire-answer-v3.0",
-        // "eachinaenglishclient",
-        // "eachinaMusicClient"
+        "eachinaenglishclient",
+        "eachinaMusicClient"
     ],
     parent: 'eachinaclientframe',
     targets: [
-        // 'linux-x64',
-        // 'linux-arm64',
-        // 'linux-loong64',
+        'linux-x64',
+        'linux-arm64',
+        'linux-loong64',
         'win-ia32'
     ]
 }
