ea112698cc95f4d12dd2668483b727da4ae493a9
lhc-up <18513102276@163.com>
2025-04-24 16:05:05 +0800

    修改配置

--- config.js
+++ config.js
@@ -3,39 +3,12 @@ const config = {
     installers: [
         {
             name: 'eachinaclientframe',
-            version: '1.3.1',
-            targets: [
-                // 'linux-x64',
-                // 'linux-arm64',
-                // 'linux-loong64',
-                'win-ia32'
-            ],
-            children: [
-                {
-                    name: "questionnaire-answer-v3.0",
-                    version: "2.6.4",
-                    // 默认develop
-                    branch: 'develop',
-                    // icon: 'builderClient/icons/128x128.png'
-                },
-                {
-                    name: 'eachinaenglishclient',
-                    version: '1.0.2'
-                },
-                {
-                    name: 'eachinaMusicClient',
-                    version: '1.3.4'
-                }
-            ]
-        },
-        {
-            name: 'eachinaclientframe',
             version: '1.4.0',
             targets: [
-                // 'linux-x64',
-                // 'linux-arm64',
-                // 'linux-loong64',
-                'win-ia32'
+                'linux-x64',
+                'linux-arm64',
+                'linux-loong64',
+                // 'win-ia32'
             ],
             children: [
                 {
@@ -54,18 +27,7 @@ const config = {
                     version: '1.3.4'
                 }
             ]
-        },
-        // {
-        //     name: 'questionnaire-answer-v3.0',
-        //     version: '2.100.3',
-        //     includeChild: false,
-        //     targets: [
-        //         // 'linux-x64',
-        //         // 'linux-arm64',
-        //         // 'linux-loong64',
-        //         'win-ia32'
-        //     ]
-        // }
+        }
     ],
 }
 
